4f032d610dd066d3d29529cd4e7c2dfbd888885e
Nathan Rajlich <nathan@tootallnate.net>
2011-08-11 20:19:40 -0700

    current index.js

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-var core = require('./core')
-  , Class = require('./class')
-  , bs = require('./bridgesupport')
-
-exports.resolve = bs.resolve;
-exports.import = bs.import;
+module.exports = require('./import')
