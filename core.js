4922f420a4b3af23ef2efb175d64f5b82f1bf67e
Nathan Rajlich <nathan@tootallnate.net>
2011-08-11 20:18:12 -0700

    more progress and fixing the objc_msgSend generator.

--- core.js
+++ core.js
@@ -4,11 +4,14 @@
  */
 
 var ffi = require('node-ffi')
+  , types = require('./types')
   // TODO: These static ffi bindings could be replaced with native bindings
   //       for a speed boost.
   , objc = new ffi.Library(null, {
-      objc_getClass: [ 'pointer', [ 'string' ] ]
-    , class_copyMethodList: [ 'pointer', [ 'pointer', 'pointer' ] ]
+      class_copyMethodList: [ 'pointer', [ 'pointer', 'pointer' ] ]
+    , class_getClassMethod: [ 'pointer', [ 'pointer', 'pointer' ] ]
+    , class_getInstanceMethod: [ 'pointer', [ 'pointer', 'pointer' ] ]
+    , class_getInstanceSize: [ 'size_t', [ 'pointer' ] ]
     , class_getName: [ 'string', [ 'pointer' ] ]
     , class_getSuperclass: [ 'pointer', [ 'pointer' ] ]
     , class_getVersion: [ 'pointer', [ 'pointer' ] ]
@@ -16,6 +19,7 @@ var ffi = require('node-ffi')
     , method_copyReturnType: [ 'pointer', [ 'pointer' ] ]
     , method_copyArgumentType: [ 'pointer', [ 'pointer', 'uint32' ] ]
     , method_getNumberOfArguments: [ 'uint32', [ 'pointer' ] ]
+    , objc_getClass: [ 'pointer', [ 'string' ] ]
     , objc_getClassList: [ 'int32', [ 'pointer', 'int32' ] ]
     , object_getClass: [ 'pointer', [ 'pointer' ] ]
     , sel_registerName: [ 'pointer', [ 'string' ] ]
@@ -34,16 +38,17 @@ exports.dlopen = function dlopen (path) {
 
 // Creates and/or returns an appropriately wrapped up 'objc_msgSend' function
 // based on the given Method description info.
-exports.get_objc_msgSend = function get_objc_msgSend (args) {
-  var type = ['pointer', 'pointer'] // id and sel
-    , types = [ objcToFfi(info.retval), type ]
+exports.get_objc_msgSend = function get_objc_msgSend (objcTypes) {
+  var type = ['pointer', 'pointer'] // id and SEL
+    , rtn = [ types.map(objcTypes[0]), type ]
+    , args = objcTypes[1]
     , i = 0
-    , l = info.args.length
+    , l = args.length
   for (; i<l; i++) {
-    type.push(objcToFfi(info.args[i]));
+    type.push(types.map(args[i]));
   }
   // Stringify the types
-  var key = types.toString();
+  var key = rtn.toString();
   console.warn('INFO: types key: %s', key);
 
   // first check the cache
@@ -52,7 +57,7 @@ exports.get_objc_msgSend = function get_objc_msgSend (args) {
 
   // If we got here, then create a new objc_msgSend ffi wrapper
   var lib = new ffi.Library(null, {
-    objc_msgSend: types
+    objc_msgSend: rtn
   })
   // return and cache at the same time
   return msgSendCache[key] = lib.objc_msgSend;
