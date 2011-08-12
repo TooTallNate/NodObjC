c148cb81f17427c80b48ebe72239b1f04c770562
Nathan Rajlich <nathan@tootallnate.net>
2011-08-12 12:19:29 -0700

    rewrite tests/import into a real test case.

--- tests/import.js
+++ tests/import.js
@@ -1,6 +1,15 @@
 var $ = require('../')
+  , assert = require('assert')
 
+var s1 = s2 = size();
 $.import('Foundation');
+s2 = size();
+assert.ok(s2 > s1);
+s1 = s2;
 $.import('ScriptingBridge');
+s2 = size();
+assert.ok(s2 > s1);
 
-console.log($);
+function size () {
+  return Object.keys($).length;
+}
