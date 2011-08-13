d8233f75d52d06ba0c571618c84deabf6b693054
Nathan Rajlich <nathan@tootallnate.net>
2011-08-13 13:50:49 -0700

    export the class.getClass() function into the id module.

--- class.js
+++ class.js
@@ -8,6 +8,8 @@ exports.getClass = function (c) {
   var pointer = core.objc_getClass(c);
   return classCache[c] = exports.wrap(pointer);
 }
+// Making the getClass function available to the id module, to avoid circular deps
+id._getClass = exports.getClass;
 
 exports.wrap = function wrap (pointer) {
   var w = id.wrap(pointer);
