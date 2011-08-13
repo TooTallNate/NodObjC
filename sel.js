b10ea571fb4f0533164edfd8c94f55995391246a
Nathan Rajlich <nathan@tootallnate.net>
2011-08-13 00:55:17 -0700

    Use 'toSEL()' and 'toString()' functions in the sel.js module.

--- sel.js
+++ sel.js
@@ -4,8 +4,19 @@
 var core = require('./core')
   , cache = {}
 
-module.exports = function SEL (sel) {
+/**
+ * Transforms a JS String selector into a SEL pointer reference.
+ * This function does caching internally.
+ */
+exports.toSEL = function toSEL (sel) {
   var rtn = cache[sel];
   if (rtn) return rtn;
   return cache[sel] = core.sel_registerName(sel);
+}
+
+/**
+ * Transforms a SEL reference to a JS String.
+ */
+exports.toString = function toString (SEL) {
+  return core.sel_getName(SEL);
 }
