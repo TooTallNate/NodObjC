f7cde754dce8f26de6160f19bc577332419b9991
Nathan Rajlich <nathan@tootallnate.net>
2011-08-14 12:01:56 -0700

    added convert() and parse() types functions.

--- types.js
+++ types.js
@@ -28,8 +28,10 @@ var map = {
   , '?': 'pointer'  // Unknown, used for function pointers
 };
 
-// Translate an Obj-C 'type' into a valid node-ffi type.
-function translate (type) {
+/**
+ * Translates a single Obj-C 'type' into a valid node-ffi type.
+ */
+exports.map = function translate (type) {
   var rtn = map[type];
   if (rtn) return rtn;
   // Meh, need better testing here
@@ -39,4 +41,25 @@ function translate (type) {
   if (type[0] === '{') return 'pointer'; // TODO: Better struct parsing
   throw new Error('Could not convert type: ' + type);
 }
-exports.map = translate;
+
+/**
+ * Accepts an Array of ObjC return type and argument types (i.e. the result of
+ * parse() below), and returns a new Array with the values mapped to valid ffi
+ * types.
+ */
+exports.convert = function convert (types) {
+  return types.map(function (type) {
+    return Array.isArray(type) ? exports.convert(type) : exports.map(type);
+  });
+}
+
+/**
+ * Parses a "types string" (i.e. "v@:") and returns a "types Array", where the
+ * return type is the first array value, and an Array of argument types is the
+ * array second value.
+ */
+exports.parse = function parse (types) {
+  if (types[1] !== '@' || types[2] !== ':')
+    throw new Error('Invalid types string: '+types)
+  return [ types[0], types.substring(1).split('') ]
+}
