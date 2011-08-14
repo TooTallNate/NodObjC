190085559f988e62f8d88975deeacbd02b36d7f1
Nathan Rajlich <nathan@tootallnate.net>
2011-08-14 10:37:25 -0700

    fix nsstring-length test

--- tests/nsstring-length.js
+++ tests/nsstring-length.js
@@ -1,10 +1,11 @@
 var $ = require('../')
-  , Foundation = $.import('Foundation')
+
+$.import('Foundation');
 
 // Create an NSAutoreleasePool
-var pool = Foundation.NSAutoreleasePool('alloc')('init')
+var pool = $.NSAutoreleasePool('alloc')('init')
 
 var str = 'Hello Objective-C!';
-var nsstr = Foundation.NSString({ 'stringWithUTF8String': str })
+var nsstr = $.NSString({ 'stringWithUTF8String': str })
 console.log('str.length:     %d', str.length);
 console.log('[nsstr length]: %d', nsstr('length'));
