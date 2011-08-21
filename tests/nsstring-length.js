5718efd2ac8f0e7b56f42e80c8d7966e1c7cb259
Nathan Rajlich <nathan@tootallnate.net>
2011-08-21 14:50:26 -0700

    Add an assert to the nsstring-length test.

--- tests/nsstring-length.js
+++ tests/nsstring-length.js
@@ -1,4 +1,5 @@
 var $ = require('../')
+  , assert = require('assert')
 
 $.import('Foundation');
 
@@ -9,3 +10,4 @@ var str = 'Hello Objective-C!';
 var nsstr = $.NSString({ 'stringWithUTF8String': str })
 console.log('str.length:     %d', str.length);
 console.log('[nsstr length]: %d', nsstr('length'));
+assert.equal(nsstr('length'), str.length)
