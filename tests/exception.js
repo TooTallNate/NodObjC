b01befd174d407596c9fab60f019a245e2d719aa
Nathan Rajlich <nathan@tootallnate.net>
2011-08-28 11:14:55 -0700

    Add some asserts to tests/exception.js.

--- tests/exception.js
+++ tests/exception.js
@@ -1,12 +1,26 @@
 var $ = require('../')
+  , assert = require('assert')
+
 $.import('Foundation')
-$.NSAutoreleasePool('alloc')('init')
+var pool = $.NSAutoreleasePool('alloc')('init')
+
 
 var array = $.NSMutableArray('alloc')('init')
+
+assert.throws(function () {
+  array('addObject', null)
+})
 
 try {
   array('addObject', null)
 } catch (e) {
-  console.error('caught exception!')
-  console.error(e.stack)
+  assert.equal('NSInvalidArgumentException', e('name'))
+  assert.ok(e.stack.length > 0)
+  assert.ok(e.message.length > 0)
+}
+
+try {
+  array('objectAtIndex', 100)
+} catch (e) {
+  assert.equal('NSRangeException', e('name'))
 }
