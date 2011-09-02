bb04371e49bb510f3b1f29494241e1fce75114b0
Nathan Rajlich <nathan@tootallnate.net>
2011-09-01 23:44:19 -0700

    Add TODO.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('./ffi-extend')
 module.exports = require('./import')
 
+// TODO: Figure out something better for this bitch.
+//         _() is just TOO ugly...
 module.exports._ = function _ (str) {
   return module.exports.NSString('stringWithUTF8String', String(str))
 }
