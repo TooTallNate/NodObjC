1c383a11a935811549875ad6ddc3169bb045841e
Nathan Rajlich <nathan@tootallnate.net>
2011-09-13 23:58:53 -0700

    Rewrite index.js
    The syntax sugar "Casting" function is now the exports directly, instead of using
    _(). This looks better imo. But the legacy _ version is still supported (as to
    not break all the tests).

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
+// Load the node-ffi extensions
 require('./ffi-extend')
-module.exports = require('./import')
 
-// TODO: Figure out something better for this bitch.
-//         _() is just TOO ugly...
-module.exports._ = function _ (str) {
+// The main exports is the casting function
+module.exports = $
+$._ = $ // legacy. TODO: remove by 0.1.0
+
+// exports the exports from the 'import' module
+var Import = require('./import')
+$.import  = Import.import
+$.resolve = Import.resolve
+
+// This function accepts native JS types (String, Number, Date) and converts them
+// to the proper Objective-C type (NSString, NSNumber, NSDate).
+//   Syntax Sugar...
+function $ (str) {
+  $.import('Foundation')
   return module.exports.NSString('stringWithUTF8String', String(str))
 }
