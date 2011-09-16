a6b22b3193e3df0860c477edfcad740383a306f7
Nathan Rajlich <nathan@tootallnate.net>
2011-09-15 22:49:15 -0700

    Add support for converting Numbers to NSNumbers and Dates to NSDates in the $ function.

--- index.js
+++ index.js
@@ -13,7 +13,20 @@ $.resolve = Import.resolve
 // This function accepts native JS types (String, Number, Date) and converts them
 // to the proper Objective-C type (NSString, NSNumber, NSDate).
 //   Syntax Sugar...
-function $ (str) {
-  $.import('Foundation')
-  return module.exports.NSString('stringWithUTF8String', String(str))
+function $ (o) {
+  var t = typeof o
+  if (t == 'string') {
+    return $.NSString('stringWithUTF8String', String(o))
+  } else if (t == 'number') {
+    return $.NSNumber('numberWithDouble', Number(o))
+  } else if (isDate(o)) {
+    return $.NSDate('dateWithTimeIntervalSince1970', o / 1000)
+  }
+  throw new Error('Unsupported object passed in to convert')
+}
+
+
+function isDate (d) {
+  return d instanceof Date
+      || Object.prototype.toString.call(d) == '[object Date]'
 }
