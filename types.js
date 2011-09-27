b8a461e4005bf4471b88a38b0747e95da23fe553
Nathan Rajlich <nathan@tootallnate.net>
2011-09-26 20:52:27 -0700

    toString() the falsey value when one is passed in.

--- types.js
+++ types.js
@@ -33,7 +33,7 @@ var map = {
  * This mapping logic is kind of a mess...
  */
 exports.map = function translate (type) {
-  if (!type) throw new Error('got falsey "type" to map. this should NOT happen!');
+  if (!type) throw new Error('got falsey "type" to map ('+type+'). this should NOT happen!');
   if (struct.isStruct(type)) return struct.getStruct(type);
   var rtn = map[type];
   if (rtn) return rtn;
