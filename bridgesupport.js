95b501ceffd2dc4cf4a9c06c631f58077e6fe965
Nathan Rajlich <nathan@tootallnate.net>
2011-09-26 21:55:54 -0700

    Almost complete rewrite of BridgeSupport parsing.
    This was mostly to implement 'function_pointer' and other C functions that
    required "special attention". This was the cause of the need for more detailed
    Type objects that the IMP and (un)wrap() functions now need to accept.

--- bridgesupport.js
+++ bridgesupport.js
@@ -6,7 +6,10 @@
  *   http://developer.apple.com/library/mac/documentation/Darwin/Reference/ManPages/man5/BridgeSupport.5.html
  */
 
-module.exports = bridgesupport;
+exports.bridgesupport = bridgesupport
+exports.classes = {}
+exports.informal_protocols = {}
+
 var fs = require('fs')
   , sax = require('sax')
   , path = require('path')
@@ -60,57 +63,67 @@ function bridgesupport (fw) {
 
   // If there's no BridgeSupport file, then just return...
   if (!exists(bridgeSupportXML)) {
-    //console.warn('No BridgeSupport files found for framework "%s" at: %s', fw.name, bridgeSupportXML);
-    return;
+    //console.warn('No BridgeSupport files found for framework "%s" at: %s', fw.name, bridgeSupportXML)
+    return
   }
 
   if (exists(bridgeSupportDylib)) {
-    fw.inline = core.dlopen(bridgeSupportDylib);
+    fw.inline = core.dlopen(bridgeSupportDylib)
   }
 
   var contents = fs.readFileSync(bridgeSupportXML, 'utf8')
     , parser = sax.parser(true)
     , gotEnd = false
-    // For processing C functions
-    , curName
-    , curRtnType
-    , curArgTypes
-    , isInline
+    , curObj = null
 
-  parser.onerror = function (e) { throw e; }
+  parser.onerror = function (e) { throw e }
   parser.onopentag = function (node) {
     var a = node.attributes
+    a._name = node.name
     switch (node.name) {
       case 'depends_on':
         Import(a.path, true)
         break;
       case 'class':
+        curObj = exports.classes[a.name] || a
+        exports.classes[curObj.name] = curObj
         break;
       case 'method':
+        a._parent = curObj
+        if (!curObj.methods)
+          curObj.methods = []
+        curObj.methods.push(a)
+        curObj = a
         break;
       case 'arg':
-        if (curName) {
-          // class methods also have args, we only want functions though
-          curArgTypes.push(getType(a));
-        }
+        // Normalize the 'type' for the platform
+        a.type = getType(a)
+        a._parent = curObj
+        if (!curObj.args)
+          curObj.args = []
+        curObj.args.push(a)
+        curObj = a
         break;
       case 'retval':
-        if (curName) {
-          // class methods also have retvals, we only want functions though
-          curRtnType = getType(a);
-        }
+        // Normalize the 'type' for the platform
+        a.type = getType(a)
+        a._parent = curObj
+        curObj.retval = a
+        curObj = a
         break;
       case 'signatures':
         break;
       case 'string_constant':
-        _global[a.name] = getValue(a);
+        _global[a.name] = getValue(a)
         break;
       case 'enum':
-        _global[a.name] = Number(getValue(a));
+        _global[a.name] = Number(getValue(a))
         break;
       case 'struct':
+        // TODO: Remove the try/catch when all the Struct formats are supported
+        //       Still need Array and Union support.
         try {
-          _global[a.name] = struct.getStruct(getType(a));
+          _global[a.name] = struct.getStruct(getType(a))
         } catch (e) {
           //console.error('FAILED:\n', a)
           //console.error(e.stack)
@@ -121,25 +134,16 @@ function bridgesupport (fw) {
       case 'cftype':
         break;
       case 'constant':
-        (function (name, type) {
-          _global.__defineGetter__(name, function () {
-            var ptr = fw.lib.get(name) // TODO: Cache the pointer after the 1st call
-            ptr._type = '^' + type
-            var val = ptr.deref()
-            return val
-          });
-        })(a.name, getType(a));
+        defineConstant(a)
         break;
       case 'function':
-        curName = a.name;
-        curRtnType = 'v';
-        curArgTypes = [];
-        isInline = a.inline == 'true';
-        // TODO: is variadic? will require a 'function generator'
+        curObj = a
         break;
       case 'opaque':
         break;
       case 'informal_protocol':
+        curObj = exports.informal_protocols[a.name] || a
+        exports.informal_protocols[curObj.name] = curObj
         break;
       case 'function_alias':
         break;
@@ -147,37 +151,72 @@ function bridgesupport (fw) {
         throw new Error('unkown tag: '+ node.name);
         break;
     }
-  };
+  }
   parser.onclosetag = function (node) {
     switch (node) {
       case 'function':
         // Binded functions will be lazy-loaded. We simply define a getter that
         // does the creation magic the first time, and replaces the getter with
         // the binded function
-        (function (curName, curRtnType, curArgTypes, isInline) {
-          if (isInline && !fw.inline) throw new Error('declared inline but could not find inline dylib!');
-          _global.__defineGetter__(curName, function () {
-            // TODO: Handle 'variadic' arg functions (NSLog), will require
-            //       a "function generator" to get a Function from the passed
-            //       in args (and guess at the types that were passed in...)
-            var ptr = (isInline ? fw.inline : fw.lib).get(curName)
-              , unwrapper = IMP.createUnwrapperFunction(ptr, [ curRtnType, curArgTypes ])
-            unwrapper.func = curName
-            unwrapper.inline = isInline
-            delete _global[curName]
-            return _global[curName] = unwrapper
-          });
-        })(curName, curRtnType, curArgTypes, isInline);
-        curName = null;
+        defineFunction(curObj, fw)
+        curObj = null
+        break;
+      case 'arg':
+      case 'method':
+      case 'retval':
+        var c = curObj._parent
+        curObj._parent = null
+        curObj = c
         break;
     }
-  };
+  }
   parser.onend = function () {
-    gotEnd = true;
-  };
+    gotEnd = true
+  }
 
   // Parse the contents of the file. This should happen synchronously.
-  parser.write(contents).close();
+  parser.write(contents).close()
 
-  if (!gotEnd) throw new Error('could not parse BridgeSupport files synchronously');
+  if (!gotEnd) throw new Error('could not parse BridgeSupport files synchronously')
+}
+
+
+/**
+ * Sets up a <constant> tag onto the global exports.
+ * These start out as simple JS getters, so that the underlying
+ * symbol pointer can be lazy-loaded on-demand.
+ */
+function defineConstant (a, fw) {
+  var name = a.name
+    , type = getType(a)
+  _global.__defineGetter__(name, function () {
+    var ptr = fw.lib.get(name) // TODO: Cache the pointer after the 1st call
+    ptr._type = '^' + type
+    var val = ptr.deref()
+    return val
+  });
+}
+
+
+/**
+ * Sets up a <function> tag onto the global exports.
+ * These start out as simple JS getters, so that the underlying
+ * function pointer can be lazy-loaded on-demand.
+ */
+function defineFunction (a, fw) {
+  var name = a.name
+    , isInline = a.inline == 'true'
+  if (isInline && !fw.inline) throw new Error(name+', '+fw.name+': declared inline but could not find inline dylib!')
+  _global.__defineGetter__(name, function () {
+    if (!a.retval) a.retval = { type: 'v' }
+    console.error(require('util').inspect(a, true, 10))
+    // TODO: Handle 'variadic' arg functions (NSLog), will require
+    //       a "function generator" to get a Function from the passed
+    //       in args (and guess at the types that were passed in...)
+    var ptr = (isInline ? fw.inline : fw.lib).get(name)
+      , unwrapper = IMP.createUnwrapperFunction(ptr, a)
+    unwrapper.info = a
+    delete _global[name]
+    return _global[name] = unwrapper
+  })
 }
