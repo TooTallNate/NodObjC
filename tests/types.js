9c349947ecdca4e0827bc86efd01fe8b47c9ef94
Nathan Rajlich <nathan@tootallnate.net>
2011-10-04 20:08:35 -0700

    Beginning to add some better tests for types.parse().

--- tests/types.js
+++ tests/types.js
@@ -3,10 +3,20 @@ var types = require('../types')
 
 var type = 'v@:'
   , parsed = types.parse(type)
-  , converted = types.convert(parsed)
+  , converted = types.mapArray(parsed)
 
 assert.deepEqual(parsed, [ 'v', [ '@', ':' ] ])
 assert.deepEqual(converted, [ 'void', [ 'pointer', 'pointer' ] ])
 
 // test types.map()
-assert.equal(types.map('r^^{__CFData}'), 'pointer')
+//assert.equal(types.map('r^^{__CFData}'), 'pointer')
+assert.equal(types.map('^^{__CFData}'), 'pointer')
+
+
+type = 'Q40@0:8^{?=Q^@^Q[5Q]}16^@24Q32';
+parsed = types.parse(type)
+console.error(parsed)
+
+type = '@68@0:8{CGRect={CGPoint=dd}{CGSize=dd}}16Q48Q56c64';
+parsed = types.parse(type)
+console.error(parsed)
