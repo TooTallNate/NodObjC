8fff3dfce9810d273c72820d13d4eb04bec34dd9
Nathan Rajlich <nathan@tootallnate.net>
2011-10-04 22:49:46 -0700

    Better types test case.

--- tests/types.js
+++ tests/types.js
@@ -1,22 +1,34 @@
 var types = require('../types')
   , assert = require('assert')
+  , inspect = require('util').inspect
 
-var type = 'v@:'
-  , parsed = types.parse(type)
-  , converted = types.mapArray(parsed)
-
-assert.deepEqual(parsed, [ 'v', [ '@', ':' ] ])
-assert.deepEqual(converted, [ 'void', [ 'pointer', 'pointer' ] ])
+test('v@:'
+  , [ 'v', [ '@', ':' ] ]
+  , [ 'void', [ 'pointer', 'pointer' ] ]
+)
 
 // test types.map()
 //assert.equal(types.map('r^^{__CFData}'), 'pointer')
 assert.equal(types.map('^^{__CFData}'), 'pointer')
 
 
-type = 'Q40@0:8^{?=Q^@^Q[5Q]}16^@24Q32';
-parsed = types.parse(type)
-console.error(parsed)
+test('Q40@0:8^{?=Q^@^Q[5Q]}16^@24Q32'
+  , [ 'Q', [ '@', ':', '^{?=Q^@^Q[5Q]}', '^@', 'Q' ] ]
+  , [ 'ulonglong', [ 'pointer', 'pointer', 'pointer', 'pointer', 'ulonglong'] ]
+)
 
-type = '@68@0:8{CGRect={CGPoint=dd}{CGSize=dd}}16Q48Q56c64';
-parsed = types.parse(type)
-console.error(parsed)
+test('@68@0:8{CGRect={CGPoint=dd}{CGSize=dd}}16Q48Q56c64'
+  , [ '@', [ '@', ':', '{CGRect={CGPoint=dd}{CGSize=dd}}', 'Q', 'Q', 'c' ] ]
+)
+
+
+function test (type, rtn, ffi) {
+  console.log('Input:\t%s', type)
+  var parsed = types.parse(type)
+  console.log('Output:\t'+inspect(parsed, true, 10, true)+'\n')
+  assert.deepEqual(parsed, rtn)
+  if (!ffi) return
+  var f = types.mapArray(parsed)
+  console.log('FFI Types:\t'+inspect(f, true, 10, true)+'\n')
+  assert.deepEqual(f, ffi)
+}
