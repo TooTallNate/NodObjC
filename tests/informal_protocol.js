8fdbca7dc957aabb78e7e1207152a3444bbad860
Nathan Rajlich <nathan@tootallnate.net>
2011-10-14 00:19:20 -0700

    Add failing addProtocol() test.

---
+++ tests/informal_protocol.js
@@ -0,0 +1,24 @@
+var $ = require('../')
+  , assert = require('assert')
+
+$.import('Foundation')
+
+var pool = $.NSAutoreleasePool('alloc')('init')
+
+var NRLock = $.NSObject.extend('NRLock')
+
+var impl = {}
+impl.lock = function lock (self, _cmd) {
+  console.error('lock being called!!!')
+}
+
+impl.unlock = function unlock (self, _cmd) {
+
+}
+
+NRLock.addProtocol('NSLocking', impl)
+
+NRLock.register()
+
+var instance = $.NRLock('alloc')('init')
+instance('lock')
