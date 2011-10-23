a4ce910bacdd4d9a857321cb3280255ae61d5acc
Nathan Rajlich <nathan@tootallnate.net>
2011-10-22 19:18:46 -0700

    Redo the NSLog test to not output to the console and use assert.

--- test/NSLog.js
+++ test/NSLog.js
@@ -1,7 +1,24 @@
 var $ = require('../')
+  , fs = require('fs')
+  , assert = require('assert')
+  , logfile = __filename + '.log'
+  , test = 'test'
+
 $.import('Foundation')
 $.NSAutoreleasePool('alloc')('init')
 
-$.NSLog($._('test'))
+fs.closeSync(process.stderr.fd)
+var fd = fs.openSync(logfile, 'w')
+
+// The result of NSLog() will be written to `logfile`
+$.NSLog($(test))
+
+fs.closeSync(fd)
+var log = fs.readFileSync(logfile, 'utf8')
+fs.unlinkSync(logfile)
+
+assert.equal(test + '\n', log.substring(log.length - test.length - 1))
+
+
 // $.NSLog($._("An object: %@"), $.NSArray('alloc')('init'))
 // Need a "function generator" to variadic functions before this will work.
