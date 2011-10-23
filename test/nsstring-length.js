7c87cecf4a5cfcdc0423b74539290ccdda8b7904
Nathan Rajlich <nathan@tootallnate.net>
2011-10-22 19:35:10 -0700

    Make test/nsstring-length.js silent.

--- test/nsstring-length.js
+++ test/nsstring-length.js
@@ -1,13 +1,10 @@
 var $ = require('../')
   , assert = require('assert')
 
-$.import('Foundation');
+$.import('Foundation')
 
-// Create an NSAutoreleasePool
 var pool = $.NSAutoreleasePool('alloc')('init')
 
-var str = 'Hello Objective-C!';
-var nsstr = $.NSString({ 'stringWithUTF8String': str })
-console.log('str.length:     %d', str.length);
-console.log('[nsstr length]: %d', nsstr('length'));
+var str = 'Hello Objective-C!'
+  , nsstr = $.NSString('stringWithUTF8String', str)
 assert.equal(nsstr('length'), str.length)
