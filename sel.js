f3b19ad2d5bcc434dac541dc93f27e7879a8cd60
Nathan Rajlich <nathan@tootallnate.net>
2011-12-03 18:14:13 -0800

    JSDoc comments to sel.js

--- sel.js
+++ sel.js
@@ -20,6 +20,10 @@ var core = require('./core')
 /**
  * Transforms a JS String selector into a SEL pointer reference.
  * This function does caching internally.
+ *
+ * @param {String} sel A String selector to turn into a native SEL pointer.
+ * @return {Pointer} The SEL pointer that was generated, or a cached version.
+ * @api private
  */
 
 function toSEL (sel) {
@@ -30,6 +34,10 @@ function toSEL (sel) {
 
 /**
  * Transforms a SEL reference to a JS String.
+ *
+ * @param {Pointer} SEL the SEL Pointer to turn into a JS String.
+ * @return {String} The String value of the given SEL.
+ * @api private
  */
 
 function toString (SEL) {
