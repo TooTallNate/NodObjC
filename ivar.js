c8298fa99451ab3e9733968181210bd23d31c3e9
Nathan Rajlich <nathan@tootallnate.net>
2011-12-03 19:23:51 -0800

    Add comments to ivar.js.

--- ivar.js
+++ ivar.js
@@ -1,29 +1,59 @@
+
 /**
  * Represents an Objective-C class "ivar", or instance variable.
+ */
+
+/**
+ * Module exports.
  */
 
 exports.wrap = wrap
 exports.Ivar = Ivar
 
+/**
+ * Module dependencies.
+ */
+
 var core = require('./core')
   , proto = Ivar.prototype
+
+/**
+ * Wraps a Pointer that should be an Objective-C ivar (instance variable), and
+ * returns a new Ivar instance.
+ */
 
 function wrap (pointer) {
   if (pointer.isNull()) return null
   return new Ivar(pointer)
 }
 
+/**
+ * The `Ivar` Class. Wrapper around an Objective-C *ivar* pointer.
+ */
+
 function Ivar (pointer) {
   this.pointer = pointer
 }
+
+/**
+ * Returns the name of the `Ivar`.
+ */
 
 proto.getName = function getName () {
   return core.ivar_getName(this.pointer)
 }
 
+/**
+ * Returns the offset of the `Ivar`.
+ */
+
 proto.getOffset = function getOffset () {
   return core.ivar_getOffset(this.pointer)
 }
+
+/**
+ * Returns the "type encoding" of the `Ivar`.
+ */
 
 proto.getTypeEncoding = function getTypeEncoding () {
   return core.ivar_getTypeEncoding(this.pointer)
