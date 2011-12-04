abcd6362394a802f24a2167e2f43693e70746fc9
Nathan Rajlich <nathan@tootallnate.net>
2011-12-03 20:59:40 -0800

    Comments for ffi-extend.js.

--- ffi-extend.js
+++ ffi-extend.js
@@ -1,3 +1,8 @@
+
+/**
+ * Module dependencies.
+ */
+
 var ffi = require('node-ffi')
   , core = require('./core')
   , types = require('./types')
@@ -5,16 +10,21 @@ var ffi = require('node-ffi')
   , SIZE_MAP = ffi.Bindings.TYPE_SIZE_MAP
   , FUNC_MAP = ffi.TYPE_TO_POINTER_METHOD_MAP
 
-// We include a polyfil for `process.arch` on node 0.4.x
-if (!process.arch) {
+/**
+ * We include a polyfil for `process.arch` on node 0.4.x.
+ */
+
+if (!('arch' in process)) {
   process.arch = ffi.Bindings.POINTER_SIZE == 8 ? 'x64' : 'ia32'
 }
 
 /**
  * Returns a new Pointer that points to this pointer.
  * Equivalent to the "address of" operator:
- *   type* = &ptr
+ *
+ *     type* = &ptr
  */
+
 Pointer.prototype.ref = function ref () {
   var ptr = new Pointer(SIZE_MAP.pointer)
   ptr.putPointer(this)
@@ -25,8 +35,10 @@ Pointer.prototype.ref = function ref () {
 /**
  * Dereferences the pointer. Includes wrapping up id instances when necessary.
  * Equivalent to the "value at" operator:
- *   type = *ptr
+ *
+ *     type = *ptr
  */
+
 Pointer.prototype.deref = function deref () {
   var t = this._type
   if (t[0] !== '^') throw new Error('cannot dereference non-pointer')
