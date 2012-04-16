9aedc61a3e9d10649e117a59bcc5c362e0ee8573
Nathan Rajlich <nathan@tootallnate.net>
2012-04-15 18:44:38 -0700

    make test/core/blocks.js test be silent

--- test/core/blocks.js
+++ test/core/blocks.js
@@ -20,7 +20,7 @@ var pool = msgSend(msgSend(NSAutoreleasePool, alloc), init)
 var set = msgSend(msgSend(NSMutableSet, alloc), init)
 
 msgSend4(set, addObject, NSMutableSet)
-console.log(msgSend2(msgSend(set, description), UTF8String));
+//console.log(msgSend2(msgSend(set, description), UTF8String))
 
 
 
@@ -33,20 +33,22 @@ var __block_literal_1 = b.Struct([
   , ['pointer', 'invoke']
   , ['pointer', 'descriptor']
 ])
-console.log(__block_literal_1.__structInfo__)
-console.log('sizeof __block_literal_1: %d', __block_literal_1.__structInfo__.size);
+//console.log(__block_literal_1.__structInfo__)
+//console.log('sizeof __block_literal_1: %d', __block_literal_1.__structInfo__.size)
 
 var __block_descriptor_1 = b.Struct([
     ['ulonglong', 'reserved']
   , ['ulonglong', 'Block_size']
 ])
-console.log('sizeof __block_descriptor_1: %d', __block_descriptor_1.__structInfo__.size);
+//console.log('sizeof __block_descriptor_1: %d', __block_descriptor_1.__structInfo__.size)
 
 // Enumerate using a block.
+var gotCallback = false
 var blockFunc = new b.Callback([ 'int8', [ 'pointer', 'pointer', 'pointer' ]], function (block, obj, stopPtr) {
-  console.error('inside block!');
-  //console.error("Enumerate: %d!", index);
-});
+  //console.error('inside block!')
+  //console.error("Enumerate: %d!", index)
+  gotCallback = true
+})
 
 var invokePtr = blockFunc.getPointer()
   , bl = new __block_literal_1
@@ -57,18 +59,22 @@ bd.reserved = 0
 bd.Block_size = __block_literal_1.__structInfo__.size
 
 bl.isa = b.dlopen().get('_NSConcreteGlobalBlock')
-console.log('isa:', bl.isa)
+//console.log('isa:', bl.isa)
 bl.flags = (1<<29)
-console.log('flags:', bl.flags)
+//console.log('flags:', bl.flags)
 bl.reserved = 0
-console.log('reserved:', bl.reserved)
+//console.log('reserved:', bl.reserved)
 bl.invoke = invokePtr
-console.log('invoke:', bl.invoke)
+//console.log('invoke:', bl.invoke)
 bl.descriptor = bd.pointer
-console.log('descriptor:', bl.descriptor)
+//console.log('descriptor:', bl.descriptor)
 
 
-console.log(bl.pointer)
-console.error(msgSend2(msgSend(bl.pointer, description), UTF8String));
+//console.log(bl.pointer)
+//console.error(msgSend2(msgSend(bl.pointer, description), UTF8String))
 
 msgSend3(set, objectsPassingTest, bl.pointer)
+
+process.on('exit', function () {
+  assert.ok(gotCallback)
+})
