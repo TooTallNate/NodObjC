85ffacc785d5128638656e2ed99b34bd9ff3a6a9
Nathan Rajlich <nathan@tootallnate.net>
2012-04-15 18:54:31 -0700

    make test/core/getClassList.js be silent

--- test/core/getClassList.js
+++ test/core/getClassList.js
@@ -1,22 +1,26 @@
 var b = require('../../lib/core')
+  , assert = require('assert')
 
 
-b.dlopen('/System/Library/Frameworks/Foundation.framework/Foundation');
+b.dlopen('/System/Library/Frameworks/Foundation.framework/Foundation')
 
 // First get the number of classes
-var numClasses = b.objc_getClassList(null, 0);
-console.error('Number of classes: %d', numClasses);
+var numClasses = b.objc_getClassList(null, 0)
+//console.error('Number of classes: %d', numClasses)
+assert.ok(numClasses > 0)
 
 if (numClasses > 0) {
   var sizeofClass = 8 // need a good way to sizeof for real
     , classes = new b.Pointer(sizeofClass * numClasses)
     , cursor = classes
 
-  b.objc_getClassList(classes, numClasses);
+  b.objc_getClassList(classes, numClasses)
 
   for (var i=0; i<numClasses; i++) {
     var c = cursor.getPointer()
-    console.error(b.class_getName(c));
+      , name = b.class_getName(c)
+    assert.equal(typeof name, 'string')
+    assert.ok(name.length > 0)
     cursor = cursor.seek(sizeofClass)
   }
 
