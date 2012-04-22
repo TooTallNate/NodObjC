34424132ea053292552cf54ced9c8b139e4797ee
Nathan Rajlich <nathan@tootallnate.net>
2012-04-21 18:22:41 -0700

    add some comments to the NSAppleScript example

--- examples/nsapplescript.js
+++ examples/nsapplescript.js
@@ -1,12 +1,21 @@
 var $ = require('../')
+
+// import the "Foundation" framework and its dependencies
 $.import('Foundation')
 
+// create the mandatory NSAutoreleasePool instance
 var pool = $.NSAutoreleasePool('alloc')('init')
 
+// create an NSString of the applescript command that will be run
 var command = $('tell (system info) to return system version')
+
+// create an NSAppleScript instance with the `command` NSString
 var appleScript = $.NSAppleScript('alloc')('initWithSource', command)
 
+// finally execute the NSAppleScript instance synchronously
 var resultObj = appleScript('executeAndReturnError', null)
+
+// resultObj may be null` or an NSAppleEventDescriptor instance , so check first
 if (resultObj) {
   console.dir(resultObj('stringValue').toString())
 }
