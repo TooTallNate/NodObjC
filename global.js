72976516fc335ace99a9a2b5ff8f45d2803c7acf
Trevor Linton <trevor.linton@gmail.com>
2014-07-20 21:50:53 -0600

    Lots of fixes, less memory used, more decoupled interface, tons of bug fixes

--- global.js
+++
@@ -1,2 +0,0 @@
-module.exports = require('./')
-global.__proto__ = require('./lib/global')
--- test/array-references.js
+++ test/array-references.js
@@ -10,4 +10,4 @@ array('addObject', $.NSArray);
 var nsarray = array('objectAtIndex', 0);
 
 assert.equal(nsarray.pointer.address, $.NSArray.pointer.address)
-assert.ok(nsarray === $.NSArray, 'fails strict equality test')
+assert.ok(nsarray === $.NSArray, 'fails strict equality test ')
