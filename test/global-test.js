b8c6c0beace9403582803cb5bacf53228254d428
Nathan Rajlich <nathan@tootallnate.net>
2014-07-31 12:55:42 -0700

    test: add `global` mode test file

--- test/global-test.js
+++ test/global-test.js
@@ -1,5 +1,15 @@
-$ = require('../lib/global'),
-	assert = require('assert');
-$.import('Foundation');
+var $ = require('../global');
+var assert = require('assert');
 
-assert.ok(NSObject, 'Cannot find nsobject.');
+// test that global `framework()` style import works
+framework('Foundation');
+
+// test that the `NSObject` class is present
+assert(NSObject, 'Cannot find global NSObject');
+
+// test that the `$` exports also contain `NSObject`
+assert($.NSObject, 'Cannot find exported NSObject');
+
+// test that `$` is *not* the global object
+foo = 'bar';
+assert($.foo !== 'bar');
