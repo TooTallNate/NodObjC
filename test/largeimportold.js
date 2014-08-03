cd0b1d91097511afc50a75fb2332451388e76b7f
Trevor Linton <trevor.linton@gmail.com>
2014-08-03 12:25:38 -0600

    Added unit tests.

---
+++ test/largeimportold.js
@@ -0,0 +1,30 @@
+var $ = require('./archive/oldimport');
+var memwatch = require('memwatch');
+var hd = new memwatch.HeapDiff();
+var hrstart = process.hrtime();
+$.import('Foundation')
+$.import('Cocoa')
+$.import('WebKit')
+$.import('AppKit')
+$.import('CoreGraphics')
+$.import('CoreFoundation')
+$.import('DebugSymbols')
+$.import('ScriptingBridge')
+$.import('AVKit')
+$.import('Accelerate')
+$.import('AddressBook')
+$.import('ApplicationServices')
+$.import('Automator')
+$.import('CFNetwork')
+$.import('CalendarStore')
+$.import('CoreAudio')
+$.import('CoreAuthentication')
+$.import('CoreAudioKit')
+$.import('CoreBluetooth')
+$.import('CoreData')
+$.import('CoreMedia')
+var hrend = process.hrtime(hrstart);
+var diff = hd.end();
+
+process.stdout.write("\033[90m memory ["+diff.change.size_bytes+' bytes]');
+process.stdout.write(" time ["+hrend[0]+"s "+(hrend[1]/1000000)+" ms] \033[0m");
