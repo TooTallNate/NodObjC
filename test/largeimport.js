44de5442c0fd2985fee393e66a480a87eb9ec533
Trevor Linton <trevor.linton@gmail.com>
2014-08-08 17:57:12 -0600

    Fix for node 0.6.0 no hrtime

--- test/largeimport.js
+++ test/largeimport.js
@@ -1,7 +1,7 @@
 var $ = require('../');
 var memwatch = require('memwatch');
 var hd = new memwatch.HeapDiff();
-var hrstart = process.hrtime();
+var hrstart = ( process.hrtime ? process.hrtime() : Date.now() )
 $.import('Foundation')
 $.import('Cocoa')
 $.import('WebKit')
@@ -21,8 +21,12 @@ $.import('CoreAudio')
 $.import('CoreAudioKit')
 $.import('CoreData')
 $.import('CoreMedia')
-var hrend = process.hrtime(hrstart);
+var hrend = ( process.hrtime ? process.hrtime(hrstart) : Date.now() );
 var diff = hd.end();
-
-process.stdout.write("\033[90m memory ["+diff.change.size_bytes+' bytes]');
-process.stdout.write(" time ["+hrend[0]+"s "+(hrend[1]/1000000)+" ms] \033[0m");
+if(process.hrtime) {
+	process.stdout.write("\033[90m memory ["+diff.change.size_bytes+' bytes]');
+	process.stdout.write(" time ["+hrend[0]+"s "+(hrend[1]/1000000)+" ms] \033[0m");
+} else {
+	process.stdout.write("\033[90m memory ["+diff.change.size_bytes+' bytes]');
+	process.stdout.write(" time ["+((hrend - hrstart)/1000000)+" ms] \033[0m");
+}
