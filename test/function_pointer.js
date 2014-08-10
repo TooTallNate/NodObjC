ed2d4060b4fee61f404bbb65b90f5a436a8798cc
Trevor Linton <trevor.linton@gmail.com>
2014-08-10 17:39:42 -0600

    Removed meta-type function_pointer from parse/wrap/unwrap

--- test/function_pointer.js
+++ test/function_pointer.js
@@ -6,7 +6,6 @@ $.import('Foundation')
 var pool = $.NSAutoreleasePool('alloc')('init')
 
 assert.equal($.NSGetUncaughtExceptionHandler.args.length, 0)
-assert.ok($.NSGetUncaughtExceptionHandler.retval.function_pointer == 'true')
 
 assert.equal($.NSSetUncaughtExceptionHandler.args.length, 1)
 assert.equal($.NSSetUncaughtExceptionHandler.retval, 'v')
