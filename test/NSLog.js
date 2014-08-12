0b431b555f5665203eaa4565d9a0f0540519c45c
Nathan Rajlich <nathan@tootallnate.net>
2014-08-11 21:11:33 -0700

    test: enable variadic testing for NSLog()

--- test/NSLog.js
+++ test/NSLog.js
@@ -2,7 +2,6 @@ var $ = require('../');
 var fs = require('fs');
 var assert = require('assert');
 var logfile = __filename + '.log';
-var test = 'test';
 
 $.import('Foundation');
 var pool = $.NSAutoreleasePool('alloc')('init');
@@ -11,14 +10,17 @@ fs.closeSync(2);
 var fd = fs.openSync(logfile, 'w');
 
 // The result of NSLog() will be written to `logfile`
-$.NSLog($(test));
+$.NSLog($('test'));
+
+var array = $.NSMutableArray('alloc')('init');
+array('addObject', $('foobar'));
+$.NSLog($("An object: %@, an NSNumber: %@"), array, $(13.3));
 
 fs.closeSync(fd);
 var log = fs.readFileSync(logfile, 'utf8');
 fs.unlinkSync(logfile);
 
-assert.equal(test + '\n', log.substring(log.length - test.length - 1));
-
-
-// $.NSLog($("An object: %@"), $.NSArray('alloc')('init'))
-// Need a "function generator" to variadic functions before this will work.
+if (! /^.* test\s*.* An object\: \(\s*foobar\s*\), an NSNumber\: 13.3\s*$/.test(log)) {
+  console.log('failed :(');
+  process.exit(1);
+}
