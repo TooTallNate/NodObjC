0a511ef815a3425e14e7c3f51c607b3a82734b4a
Trevor Linton <trevor.linton@gmail.com>
2014-09-01 01:33:06 -0600

    unittests: fixed private equal check to indexof

--- test/voidconstreturn.js
+++ test/voidconstreturn.js
@@ -6,6 +6,6 @@ var pool = $.NSAutoreleasePool('alloc')('init');
 var result = $.CGWindowListCopyWindowInfo($.kCGWindowListOptionAll, $.kCGNullWindowID);
 var windowList = $.CFBridgingRelease(result);
 
-assert(windowList.getName() == '__NSArrayM');
+assert(windowList.getName().toString().indexOf('Array') > -1);
 
 pool('drain');
