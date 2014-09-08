0b88c4e9edf1884fb9e5d34d921cce9a77d4edd9
Trevor Linton <trevor.linton@gmail.com>
2014-09-07 23:14:49 -0600

    Added ref error/alloc to unit tests

--- test/voidconstreturn.js
+++ test/voidconstreturn.js
@@ -8,4 +8,10 @@ var windowList = $.CFBridgingRelease(result);
 
 assert(windowList.getName().toString().indexOf('Array') > -1);
 
+var error = $.alloc($.NSError).ref();
+var jsonData = $.NSJSONSerialization("dataWithJSONObject", windowList, "options", $.NSJSONWritingPrettyPrinted, "error", error);
+var jsonString = ($.NSString("alloc")("initWithData", jsonData, "encoding", $.NSUTF8StringEncoding)).toString();
+
+assert(jsonString.length > 1);
+
 pool('drain');
