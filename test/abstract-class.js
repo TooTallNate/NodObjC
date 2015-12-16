59e37acff803c723ba07c9907461c5cf8a97f25a
Nathan Rajlich <nathan@tootallnate.net>
2015-12-16 14:55:10 -0800

    test: fix formatting on abstract-class test

--- test/abstract-class.js
+++ test/abstract-class.js
@@ -1,11 +1,12 @@
-$ = require('../'), assert = require('assert');
+const $ = require('../');
+const assert = require('assert');
 
 $.import('Foundation');
 
-var appSchema = $.NSURLProtocol.extend('URLAppSchema');
 var success = false;
+var appSchema = $.NSURLProtocol.extend('URLAppSchema');
 
-// on the class
+// methodsd on the class
 appSchema.addClassMethod('canInitWithRequest:', 'c@:@', function(self, cmd, theRequest) {
     success = true;
     return $.NO;
@@ -14,23 +15,31 @@ appSchema.addClassMethod('canonicalRequestForRequest:', '@@:@', function(self, c
     return request;
 });
 
-// on the classes instance
+// methods on the instances
 appSchema.addMethod('startLoading','v@:', function(self, cmd) {
 });
 
 appSchema.addMethod('stopLoading','v@:', function(self, cmd) {
 });
 
+// finalize the class (new methods can still be added, but no new
+// ivars since the size of the class is now fixed)
 appSchema.register();
 
-$.NSURLProtocol('registerClass',$.URLAppSchema('class'));
+$.NSURLProtocol('registerClass', $.URLAppSchema('class'));
 
 
-var theRequest=$.NSURLRequest('requestWithURL', $.NSURL('URLWithString', $('app://test')),
-                        'cachePolicy',$.NSURLRequestUseProtocolCachePolicy,
-                    'timeoutInterval',60.0);
-var theConnection=$.NSURLConnection('alloc')('initWithRequest',theRequest,'delegate',appSchema);
+var theRequest = $.NSURLRequest(
+    'requestWithURL', $.NSURL('URLWithString', $('app://test')),
+    'cachePolicy', $.NSURLRequestUseProtocolCachePolicy,
+    'timeoutInterval', 60.0
+  );
 
-setTimeout(function() {
-    assert.ok(success, 'The app schema was not registered.');
-},1000);
+var theConnection = $.NSURLConnection('alloc')(
+    'initWithRequest', theRequest,
+    'delegate', appSchema
+  );
+
+setTimeout(function () {
+  assert.ok(success, 'The app schema was not registered.');
+}, 1000);
