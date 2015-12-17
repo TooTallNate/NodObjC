39c481c292d640bfa8461c4012df91cede93caff
Nathan Rajlich <nathan@tootallnate.net>
2015-12-16 16:28:08 -0800

    refactor of "Exception" class
    Now the `name` property gets properly set on the exception
    instance, making the `Error#toString()` override unnecessary
    so that is removed as well. Proper inheritance chain is set
    up so that `.getClassName()` and other `ID.prototype` functions
    work as expected.

--- test/exception.js
+++ test/exception.js
@@ -1,30 +1,62 @@
-var $ = require('../')
-  , assert = require('assert')
+var $ = require('../');
+var assert = require('assert');
 
-$.import('Foundation')
-var pool = $.NSAutoreleasePool('alloc')('init')
+$.import('Foundation');
+var pool = $.NSAutoreleasePool('alloc')('init');
 
+var e1, e2;
+var array = $.NSMutableArray('alloc')('init');
 
-var array = $.NSMutableArray('alloc')('init')
+try {
+  array('addObject', null);
+} catch (_e) {
+  e1 = _e;
+}
+
+assert.equal('function', typeof e1);
+assert.equal('NSInvalidArgumentException', e1('name'));
+assert.equal('NSInvalidArgumentException', e1.name);
+assert.equal('function', typeof e1.msgSend);
+assert.equal('function', typeof e1.getClassName);
+assert.equal('NSException', e1.getClassName());
+assert.ok(e1.stack.length > 0);
+assert.ok(e1.message.length > 0);
+assert.ok(e1 instanceof e1.constructor);
+assert.equal(e1.getClass(), $.NSException);
 
 assert.throws(function () {
-  array('addObject', null)
-})
+  e1('raise');
+});
+
+
 
 try {
-  array('addObject', null)
-} catch (e) {
-  assert.equal('NSInvalidArgumentException', e('name'))
-  assert.ok(e.stack.length > 0)
-  assert.ok(e.message.length > 0)
+  array('objectAtIndex', 100);
+} catch (_e) {
+  e2 = _e;
 }
 
-try {
-  array('objectAtIndex', 100)
-} catch (e) {
-  assert.equal('NSRangeException', e('name'))
+assert.equal('function', typeof e2);
+assert.equal('NSRangeException', e2('name'));
+assert.equal('NSRangeException', e2.name);
+assert.equal('function', typeof e2.msgSend);
+assert.equal('function', typeof e2.getClassName);
+assert.equal('NSException', e2.getClassName());
+assert.ok(e2.stack.length > 0);
+assert.ok(e2.message.length > 0);
+assert.ok(e2 instanceof e2.constructor);
+assert.equal(e2.getClass(), $.NSException);
 
-  assert.throws(function () {
-    e('raise')
-  })
-}
+assert.throws(function () {
+  e2('raise');
+});
+
+
+// at this point, `e1.name` should still be intact
+// (see https://github.com/TooTallNate/node-function-name/issues/3)
+assert.equal('NSInvalidArgumentException', e1.name,
+    'most recent exception name is being cached!');
+assert.equal('NSRangeException', e2.name,
+    'most recent exception name is being cached!');
+
+pool('release');
