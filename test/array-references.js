ad22eae99ba8653e967d2eac6372c441679e550a
Nathan Rajlich <nathan@tootallnate.net>
2015-12-30 23:09:10 -0800

    fix direct JS references for *all* objc_objects
    This causes a memory-cleanup issue, but I'll save that
    for another day. Correctness over optimizations IMO.

--- test/array-references.js
+++ test/array-references.js
@@ -5,9 +5,19 @@ $.import('Foundation')
 var pool = $.NSAutoreleasePool('alloc')('init');
 
 var array = $.NSMutableArray('arrayWithCapacity', 2);
+assert.equal(array('count'), 0);
+
 array('addObject', $.NSArray);
+assert.equal(array('count'), 1);
+
+var o = $.NSObject('alloc')('init');
+array('addObject', o);
+assert.equal(array('count'), 2);
 
 var nsarray = array('objectAtIndex', 0);
-
-assert.equal(nsarray.pointer.address, $.NSArray.pointer.address)
+assert.equal(nsarray.pointer.address(), $.NSArray.pointer.address())
 assert.ok(nsarray === $.NSArray, 'fails strict equality test')
+
+var o2 = array('objectAtIndex', 1);
+assert.equal(o2.pointer.address(), o.pointer.address())
+assert.ok(o === o2, 'fails strict equality test')
